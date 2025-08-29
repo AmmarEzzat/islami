d6e56cb2f996ccc51407d144467cebda85183ad0
AmmarEzzat <your_email@example.com>
2025-08-29 14:37:37 +0300

    initial project. added empty home screen.

---
+++ lib/Core/Colors/AppColors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class AppColors{
+
+
+
+  static const Color White=Color(0xFFFFFFFF) ;
+  static const Color Black=Color(0xFF202020) ;
+  static const Color Gold=Color(0xFFE2BE7F) ;
+  static const Color Gray=Color(0xFF707070) ;
+  static const Color OfWhite=Color(0xFFFFF5E3) ;
+  static const Color Brown=Color(0xFF946107) ;
+
+
+
+}
